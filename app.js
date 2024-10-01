b73d5acb8a6840725e30ce942057e6d0f4b1ece9
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-10-01 12:14:36 +0600

    functions added

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ function loadData(){
     .then(res => res.json())
     .then(data => console.log(data));
 }
+
+const loadUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => displayUsers(data))
+}
+
+const displayUsers = (data) => {
+    console.log(data);
+}
+
+// console.log(typeof loadUsers);
